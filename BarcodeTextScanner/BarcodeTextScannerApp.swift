4f2197d36e69a37bf9252d1f27842e999966417a
TemaTerbi <tematerbi@mail.ru>
2023-01-26 21:09:08 +0300

    Initial Commit

---
+++ BarcodeTextScanner/BarcodeTextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextScannerApp.swift
+//  BarcodeTextScanner
+//
+//  Created by Артем Соловьев on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeTextScanner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BarcodeTextScanner
+//
+//  Created by Артем Соловьев on 26.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
